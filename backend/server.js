47ac92cd9d12613391eaba632a06ebc95d2b47ed
Sushmita Ahlawat <susahlawat1998@gmail.com>
2020-08-12 17:32:53 +0530

    WINKEL

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import data from './data';
+import dotenv from 'dotenv';
+import config from './config';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import userRoute from './routes/userRoute';
+
+dotenv.config();
+//bodyparser is a middleware for express that provide data that user post request into user node application.
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).catch(error => console.log(error.reason));
+
+const app = express();
+app.use(bodyParser.json());
+app.use("/api/users", userRoute);
+//app.use('/api/products', productRoute);
+app.get("/api/products/:id", (req, res) => {
+    const productId = req.params.id;
+    const product = data.products.find(x => x.id == productId);
+    if(product){
+        res.send(product);
+    }else{
+        res.status(404).send({msg: "Product Not Found"});
+    }
+    
+});
+
+app.get("/api/products", (req, res) => {
+    res.send(data.products);
+});
+
+app.listen(5000, () => {console.log("server started at http://localhost:5000")})//listen command use to start express.js server.
